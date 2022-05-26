4d7b3824599ca6fa9300b4e2c10e5d10bfe43e67
Primetraineesvn <primetraineesvn@gmail.com>
2022-05-26 23:04:22 +0100

    Rest Assured Project Skeleton

---
+++ src/test/java/com/studentapp/testbase/TestBase.java
@@ -0,0 +1,14 @@
+package com.studentapp.testbase;
+
+import org.junit.BeforeClass;
+
+/**
+ * Created by Jay Vaghani
+ */
+public class TestBase {
+
+    @BeforeClass
+    public static void inIt() {
+
+    }
+}
